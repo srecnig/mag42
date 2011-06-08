c41c14faf737df0e915c2a71c783c3a81afc1d86
martin sereinig <srecnig@fsinf.at>
2011-06-08 12:14:00 +0200

    added custom swipe gesture file

---
+++ javascript/CustomSwipeGesture.js
@@ -0,0 +1,45 @@
+Ext.gesture.CustomSwipeGesture = Ext.extend(Ext.gesture.Gesture, {
+ 
+  /* Custom Swipe Gesture Handler which adds 'Up' and 'Down' to the default 'Left' and 'Right' */
+    listenForEnd: false,
+ 
+    swipeThreshold: 35,
+    swipeTime: 1000,
+ 
+    onTouchStart : function(e, touch) {
+        this.startTime = e.timeStamp;
+        this.startX = touch.pageX;
+        this.startY = touch.pageY;
+        this.lock('scroll', 'scrollstart', 'scrollend');
+    },
+ 
+    onTouchMove : function(e, touch) {
+ 
+        var deltaY = touch.pageY - this.startY,
+            deltaX = touch.pageX - this.startX,
+            absDeltaY = Math.abs(deltaY),
+            absDeltaX = Math.abs(deltaX),
+            deltaTime = e.timeStamp - this.startTime;
+ 
+        // If the swipeTime is over, we are not gonna check for it again
+        if (deltaTime > this.swipeTime) {
+            this.unlock('scroll', 'scrollstart', 'scrollend');
+            this.stop();
+        }
+        else if (absDeltaX > this.swipeThreshold && absDeltaX > absDeltaY) {
+           // If this is a swipe, a scroll is not possible anymore
+           this.fire('swipe', e, {
+               direction: (deltaX < 0) ? 'left' : 'right',
+               distance: absDeltaX,
+               deltaTime: deltaTime,
+               deltaX: deltaX,
+               startX: touch.pageX,
+               startY: touch.pageY,
+           });
+ 
+           this.stop();
+        }
+    }
+});
+ 
+Ext.regGesture('swipe', Ext.gesture.CustomSwipeGesture);
